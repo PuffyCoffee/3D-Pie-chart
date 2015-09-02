4fcda7c27ec4ef913689b494a073fb1d5632e7b5
Peng <nj.pengzhang@gmail.com>
2015-09-01 23:20:44 -0700

    can adjust viewing angle

--- js/pie.js
+++ js/pie.js
@@ -3,13 +3,14 @@
  * @author Peng Zhang 2012
  */
 Raphael.fn.pie = function(args) {
-	var values = args.values;	
+	var values = args.values;
 	var colors = args.colors;
 	var tooltips = args.tooltip;
+	var viewAngle = translateTiltAngle(args.tilt);
 	var percentages = [], isDoughnut = args.doughnut, doughnutSize = 2.5;
 	var sum = 0;
 	for (var i = 0; i < values.length; i += 1)  {
-		sum += values[i];		
+		sum += values[i];
 	}
 	for (var j = 0; j < values.length; j += 1) {
 		percentages.push(((values[j]/sum)*100).toFixed(2)+"%");
@@ -20,17 +21,17 @@ Raphael.fn.pie = function(args) {
 		var pie_half_width = paper.width/2,
 			pie_half_height = paper.height,
 			pie_radius = (pie_half_width > pie_half_height) ? 1/3*pie_half_height : 1/3*pie_half_width;
-		r1 = pie_radius; r2 = .8*r1;
+		r1 = pie_radius; r2 = viewAngle*r1;
 		cx = pie_half_width/2; cy = pie_half_height/2;
 		titleX = cx; titleY = paper.height/13;
 
 		// Draw legend
 		var legend_half_width = paper.width/2,
 			legend_half_height = paper.height;
-		
+
 		var legend_items = args.legend.items,
 			legend_items_length = legend_items.length,
-			startX = 7/6*legend_half_width, 
+			startX = 7/6*legend_half_width,
 			stepH = legend_half_height/(legend_items_length+2),
 			startY = stepH;
 
@@ -44,19 +45,19 @@ Raphael.fn.pie = function(args) {
 				"font-size": 14
 			});
 		}
-		
+
 
 
 	} else { // just a single pie chart
 		var pie_half_width = paper.width/2,
 			pie_half_height = paper.height,
 			pie_radius = (pie_half_width > pie_half_height) ? 1/3*pie_half_height : 1/3*pie_half_width;
-		r1 = pie_radius; r2 = .8*r1; 
+		r1 = pie_radius; r2 = .8*r1;
 		cx = paper.width/2; cy = paper.height/2;
 		titleX = cx; titleY = paper.height/13;
 	}
 
-	
+
 	var title = paper.text(titleX, titleY, args.chartTitle).attr({
 		'font-size' : 20
 	});
@@ -73,7 +74,7 @@ Raphael.fn.pie = function(args) {
 				stroke: "#999",
 				opacity: .8
 			});
-			var c = paper.ellipse(cx, cy, r1/doughnutSize, r2/doughnutSize).attr({				
+			var c = paper.ellipse(cx, cy, r1/doughnutSize, r2/doughnutSize).attr({
 				stroke: "#999",
 				fill: "#fff"
 			});
@@ -86,7 +87,7 @@ Raphael.fn.pie = function(args) {
 			var arcBot = paper.path("M"+(cx+r1/doughnutSize)+","+(cy+17)+
 						 "A"+r1/doughnutSize+","+r2/doughnutSize+",0,1,1,"+
 						 (cx-r1/doughnutSize)+","+(cy+17)).attr({stroke: "#111", opacity:.2});
-			
+
 		} else {
 			var e = paper.ellipse(cx, cy, r1, r2).attr({
 				fill: colors[0],
@@ -97,8 +98,8 @@ Raphael.fn.pie = function(args) {
 				fill: colors[0],
 				stroke: "#999"
 			}).toBack();
-		}		
-	} else {	
+		}
+	} else {
 		var x = cx + r1, y = cy, preAngle = 0;
 		var ix = cx + r1/doughnutSize, iy = cy;
 		var all_set = [];
@@ -112,26 +113,26 @@ Raphael.fn.pie = function(args) {
 				y : tt.getBBox().y - 5,
 				width : tt.getBBox().width + 20,
 				height : tt.getBBox().height + 10
-			});		
+			});
 		}
 		tt.hide(), tbox.hide();
-		for (var i = 0; i < nos; i += 1) {		
-			var slice = paper.set();			
+		for (var i = 0; i < nos; i += 1) {
+			var slice = paper.set();
 			var startX = x, startY = y;
 			var iStartX = ix, iStartY = iy;
-			var ratio = values[i]/sum;		
+			var ratio = values[i]/sum;
 			var angle = 2*ratio*Math.PI, largeArc = 0;
 			angles.push(angle);
 			if (angle > Math.PI)	largeArc = 1;
 			x = cx + r1*Math.cos(angle+preAngle);
 			y = cy + r2*Math.sin(angle+preAngle);
 			ix = cx + r1/doughnutSize*Math.cos(angle+preAngle);
-			iy = cy + r2/doughnutSize*Math.sin(angle+preAngle);			
+			iy = cy + r2/doughnutSize*Math.sin(angle+preAngle);
 			var centerX = (startX+x)/2, centerY = (startY+y)/2;
 			var centerLine = paper.path("M"+cx+","+cy+"L"+centerX+","+centerY).attr({
 				stroke: "none"
 			});
-			var coord = centerLine.getPointAtLength(15);		
+			var coord = centerLine.getPointAtLength(15);
 			tset.push(coord);
 			var ttx = cx + (r1+45)*Math.cos(angle/2+preAngle),
 				tty = cy + (r2+45)*Math.sin(angle/2+preAngle);
@@ -168,7 +169,7 @@ Raphael.fn.pie = function(args) {
 				var side3 = paper.path("M"+startX+","+startY),
 					side2 = paper.path("M"+cx+","+cy),
 					side1 = paper.path("M"+cx+","+cy),
-					topside = paper.path("M"+x+","+y);		
+					topside = paper.path("M"+x+","+y);
 				side3 = paper.path("M"+startX+","+startY+
 						"A"+r1+","+r2+",0,"+largeArc+",1,"+x+","+y+
 						"L"+x+","+(y+dsize)+
@@ -185,23 +186,48 @@ Raphael.fn.pie = function(args) {
 						"A"+r1+","+r2+",0,"+largeArc+",0,"+startX+","+startY+
 						"L"+cx+","+cy+"z");
 				slice.push(topside, side1, side2, side3);
-				
-			}	
+
+			}
 			topSet.push(topside);
 			topSet.toFront();
 			slice.attr({
 				fill: colors[i],
 				stroke: "#555",
 				opacity: .8
-			});		
+			});
 			all_set.push(slice);
-			preAngle += angle;		
+			preAngle += angle;
 		}
 		var mlist = [];
 		for (var i = 0; i < values.length; i += 1) {
 			mlist[i] = false;
 		}
-		function showTooltip(index) {			
+		function translateTiltAngle(symbol) {
+			var angle = 0.8;
+			if (typeof symbol === "string") {
+				switch (symbol) {
+					case "x":
+						angle = 0.9;
+						break;
+					case "l":
+						angle = 0.8;
+						break;
+					case "m":
+						angle = 0.7;
+						break;
+					case "s":
+						angle = 0.6;
+						break;
+					default:
+						angle = 0.8;
+				}
+			} else {
+				console.log("Please pass a character[x(extra), l(large), m(medium), s(small)]"+
+							" to pie chart to adjust tilt angle.");
+			}
+			return angle;
+		}
+		function showTooltip(index) {
 			if (typeof tooltips !== 'undefined') {
 				if (ttset[index].x < cx) {
 					tt.attr({
@@ -219,7 +245,7 @@ Raphael.fn.pie = function(args) {
 					});
 				}
 				moveBox(tt);
-				tbox.attr({stroke: colors[index]});		
+				tbox.attr({stroke: colors[index]});
 				tt.toFront(), tbox.toFront();
 				tt.show(), tbox.show();
 			}
@@ -240,7 +266,7 @@ Raphael.fn.pie = function(args) {
 					setTimeout(function() {
 						all_set[i].animate({
 							transform: "t0,0"
-						});	
+						});
 						hideTooltip();
 					}, 2000);
 				}
@@ -249,7 +275,7 @@ Raphael.fn.pie = function(args) {
 				all_set[i].animate({
 					transform: "t0,0"
 				}, 300);
-			});		
-		});	
+			});
+		});
 	}
 };

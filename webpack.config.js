a27e12920795ac7b1292bec8240e1e53a3b41685
Peng Zhang <nj.pengzhang@gmail.com>
2020-06-01 17:07:01 -0700

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '3d-pie.js',
+    filename: '3d-pie.bundle.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'head'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 };
